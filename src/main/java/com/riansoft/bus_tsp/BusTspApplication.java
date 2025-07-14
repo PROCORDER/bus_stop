8c8cae613c69650b99dd3c7aa1b2298dbc1da47c
choijunwon <apro500@naver.com>
2025-07-14 13:23:56 +0900

    java spring initializer

---
+++ src/main/java/com/riansoft/bus_tsp/BusTspApplication.java
@@ -0,0 +1,13 @@
+package com.riansoft.bus_tsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riansoft/bus_tsp/BusTspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riansoft.bus_tsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
