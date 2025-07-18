415da6f71c6bd6d9ce5c984ec78493c89cf67909
choijunwon <apro500@naver.com>
2025-07-18 11:15:52 +0900

    테스트 규모를 낮춤

--- src/main/java/com/riansoft/bus_tsp/controller/RouteController.java
+++ src/main/java/com/riansoft/bus_tsp/controller/RouteController.java
@@ -4,7 +4,7 @@ import com.riansoft.bus_tsp.dto.RouteSolutionDto;
 import com.riansoft.bus_tsp.service.RouteOptimizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping; // PostMapping -> GetMapping
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +19,11 @@ public class RouteController {
         this.routeService = routeService;
     }
 
+
     @GetMapping("/optimize-route")
     public ResponseEntity<RouteSolutionDto> getOptimalRoutes() {
         RouteSolutionDto solution = routeService.findOptimalRoutes();
+
         if (solution == null) {
             return ResponseEntity.internalServerError().body(null);
         }
