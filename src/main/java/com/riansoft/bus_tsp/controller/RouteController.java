e92a58b907fc51843c005cc64019f18352e54102
choijunwon <apro500@naver.com>
2025-07-18 09:05:45 +0900

    메인 컨트롤러

---
+++ src/main/java/com/riansoft/bus_tsp/controller/RouteController.java
@@ -0,0 +1,30 @@
+package com.riansoft.bus_tsp.controller;
+
+import com.riansoft.bus_tsp.dto.RouteSolutionDto;
+import com.riansoft.bus_tsp.service.RouteOptimizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class RouteController {
+
+    private final RouteOptimizationService routeService;
+
+    @Autowired
+    public RouteController(RouteOptimizationService routeService) {
+        this.routeService = routeService;
+    }
+
+    @GetMapping("/optimize-route")
+    public ResponseEntity<RouteSolutionDto> getOptimalRoutes() {
+        RouteSolutionDto solution = routeService.findOptimalRoutes();
+        if (solution == null) {
+            return ResponseEntity.internalServerError().body(null);
+        }
+        return ResponseEntity.ok(solution);
+    }
+}
