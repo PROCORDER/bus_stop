a24d4722796da7a1ee38e41df6f6e385e21a8ebe
choijunwon <apro500@naver.com>
2025-07-30 17:06:10 +0900

    RouteModificationRequestDto 수정 파라미터를 받기위한 DTO 수정

--- src/main/java/com/riansoft/bus_tsp/dto/RouteModificationRequestDto.java
+++ src/main/java/com/riansoft/bus_tsp/dto/RouteModificationRequestDto.java
@@ -1,11 +1,21 @@
 package com.riansoft.bus_tsp.dto;
 
 import java.util.List;
-
+import java.util.Map;
 // 수정된 모든 경로 목록을 한번에 담는 최상위 DTO
 public class RouteModificationRequestDto {
     private List<ModifiedRouteDto> modifications;
+    private Map<String, String> params;
 
+
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
+    }
     // Getters and Setters
     public List<ModifiedRouteDto> getModifications() { return modifications; }
     public void setModifications(List<ModifiedRouteDto> modifications) { this.modifications = modifications; }
